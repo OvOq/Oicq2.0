78880ea0e987cb7145eb26bfbd2cc60ef0d55ec5
Vertical923 <1273683884@qq.com>
2019-08-30 11:04:30 +0800

    登录界面

--- OICQ/main.cpp
+++ OICQ/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "checklogininput.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-
-    return a.exec();
+    /*以下是登录页面*/
+    CheckLoginInput dlg;
+    if (dlg.exec() == QDialog::Accepted)
+    {
+        w.show();//展示MainWindow
+        return a.exec();
+    }
+    else
+        return 0;
 }
